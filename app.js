da8dfee2e8afc5a83d9f757a03e9b3adc3f4103a
skcreationz <selvakumard274@gmail,com>
2023-03-25 07:17:43 +0530

    chages by selva

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const ejs=require('ejs');
+const mysql=require('mysql');
+const { urlencoded } = require('express');
 require('./auth');
 
 const app = express();
@@ -13,6 +15,8 @@ function isLoggedIn(req, res, next) {
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.set('view engine','ejs');
 app.use(express.static("stylesheet"))
 app.get('/', (req, res) => {
@@ -29,15 +33,49 @@ app.get( '/auth/google/callback',
     failureRedirect: '/auth/google/failure'
   })
 );
+//Database
+// const pool=mysql.createPool({
+//   connectionLimit:10,
+//   host:'localhost',
+//   user:"root",
+//   password:"",
+//   database:"cv"
+// });
 
+
+
+let name;
+let image_src;
+let id
 app.get('/home', isLoggedIn, (req, res) => {
-  res.render('home',{name:req.user.displayName,img_src:req.user.picture});
+  name=req.user.displayName;
+  image_src=req.user.picture;
+  id:req.user.id;
+  console.log(name);
+//   pool.getConnection((err,connection)=>{
+//     if(err)throw err
+  
+//     connection.query("UPDATE homedetails set Name=? ,img=? where=?",[name,image_src,id],(err,row)=>{
+//         connection.release();
+//         if(!err){
+//                     console.log("update successfully!")
+//         }else{
+//             console.log(err)
+//         }
+//     });
+
+// })
+  res.render('home',{name:name,img_src:image_src});
 });
+
+
+
 app.get('/projects', isLoggedIn, (req, res) => {
-  res.render('projects',{name:req.user.displayName,img_src:req.user.picture});
+  
+  res.render('projects',{name:name,img_src:image_src});
 });
 app.get('/certification', isLoggedIn, (req, res) => {
-  res.render('certification',{name:req.user.displayName,img_src:req.user.picture});
+  res.render('certification',{name:name,img_src:image_src});
 });
 
 app.get('/logout', (req, res) => {
--- auth.js
+++ auth.js
@@ -19,6 +19,6 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(user, done) {
-    
+    console.log(user)
   done(null, user);
 });
